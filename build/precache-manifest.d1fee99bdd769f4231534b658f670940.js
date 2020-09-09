488a79bf571c5375002a2692dfda8f4ed86b606a
Rashmi Gupta <rashmi224141@gmail.com>
2020-09-10 01:28:48 +0530

    added build for todoapp

---
+++ build/precache-manifest.d1fee99bdd769f4231534b658f670940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e0eb98b3f30150fb5e17d99833b79e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73fae15d6e43368235a3",
+    "url": "/static/css/main.686d698e.chunk.css"
+  },
+  {
+    "revision": "5d4e4fb97223ad0e528b",
+    "url": "/static/js/2.a6636ceb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6636ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73fae15d6e43368235a3",
+    "url": "/static/js/main.c9c7d9b8.chunk.js"
+  },
+  {
+    "revision": "c0fa6ca7d1f064a94e45",
+    "url": "/static/js/runtime-main.2e8bd11b.js"
+  }
+]);
