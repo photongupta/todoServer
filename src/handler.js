086d400e2cd88328a433ffb9438a4ab7c2d495d5
Rashmi Gupta <rashmi224141@gmail.com>
2020-09-10 10:18:09 +0530

    modified handlers

--- src/handler.js
+++ src/handler.js
@@ -8,8 +8,7 @@ const attachTodoDetails = (req, res, next) => {
     .then((todoDetails) => {
       req.app.locals.todoDetails = todoDetails || getDefaultTodoDetails();
     })
-    .then(() => next())
-    .catch(() => res.end(404));
+    .then(() => next());
 };
 
 const getTodoDetails = (req, res) => {
@@ -23,9 +22,7 @@ const addTask = (req, res) => {
     id: todoDetails.lastId++,
     status: getDefaultStatus(),
   });
-  db.setTodoDetails(todoDetails)
-    .then(() => res.end())
-    .catch(() => res.end(404));
+  db.setTodoDetails(todoDetails).then(() => res.end());
 };
 
 const removeTask = (req, res) => {
@@ -33,17 +30,13 @@ const removeTask = (req, res) => {
   todoDetails.todoList = todoDetails.todoList.filter(
     (todo) => todo.id !== req.body.id
   );
-  db.setTodoDetails(todoDetails)
-    .then(() => res.end())
-    .catch(() => res.end(404));
+  db.setTodoDetails(todoDetails).then(() => res.end());
 };
 
 const resetTodoDetails = (req, res) => {
   const {db} = req.app.locals;
   const todoDetails = getDefaultTodoDetails();
-  db.setTodoDetails(todoDetails)
-    .then(() => res.end())
-    .catch(() => res.end(404));
+  db.setTodoDetails(todoDetails).then(() => res.end());
 };
 
 const updateStatus = (req, res) => {
@@ -52,17 +45,13 @@ const updateStatus = (req, res) => {
   const todo = newTodoList.find((task) => task.id === req.body.id);
   todo.status = getNextStatus(todo.status);
   todoDetails.todoList = newTodoList;
-  db.setTodoDetails(todoDetails)
-    .then(() => res.end())
-    .catch(() => res.end(404));
+  db.setTodoDetails(todoDetails).then(() => res.end());
 };
 
 const updateTitle = (req, res) => {
   const {todoDetails, db} = req.app.locals;
   todoDetails.title = req.body.title;
-  db.setTodoDetails(todoDetails)
-    .then(() => res.end())
-    .catch(() => res.end(404));
+  db.setTodoDetails(todoDetails).then(() => res.end());
 };
 
 module.exports = {
